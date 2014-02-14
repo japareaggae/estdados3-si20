874f9697a74d4ccf226b5c0bb8d758f9b41165c8
Renan Guilherme <japareaggae@gmail.com>
2014-02-14 04:05:43 -0400

    aponta.c: better comments

--- aponta.c
+++ aponta.c
@@ -1,24 +1,29 @@
 #include <stdio.h>
 
 main(){
+	// Declaração das variáveis normais
 	int var1 = 3;
 	int var2 = 1;
 	int var3 = 0;
 	int soma;
-	// int soma = var1 + var2 + var3;
 
+	// Declaração dos ponteiros. Perceba o asterisco no int*
 	int* ponteiro1;
 	int* ponteiro2;
 	int* ponteiro3;
 	int* ponteirosoma;
 
+	// Valor da variável ponteiro = Endereço de memória da variável normal
+	// (perceba o & na frente)
 	ponteiro1 = &var1;
 	ponteiro2 = &var2;
 	ponteiro3 = &var3;
 	ponteirosoma = &soma;
 
+	// Some as variáveis pra onde ponteiro1, 2, 3 apontam e salve onde ponteirosoma aponta
 	*ponteirosoma = *ponteiro1 + *ponteiro2 + *ponteiro3;
 
+	// Imprime tudo
 	printf("ponteiro1 aponta para %p (valor %d)\n", ponteiro1, *ponteiro1);
 	printf("ponteiro2 aponta para %p (valor %d)\n", ponteiro2, *ponteiro2);
 	printf("ponteiro3 aponta para %p (valor %d)\n", ponteiro3, *ponteiro3);
