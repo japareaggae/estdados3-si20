174433e198db4f5ff92e49befc22e5691a89b3e6
Renan Guilherme <japareaggae@gmail.com>
2014-02-22 15:01:49 -0400

    02-matrizes: tamanho da matriz é uma variável
    E agora usa exit(); ao invés de return. (requer stdlib.h)

--- 02-matrizes.c
+++ 02-matrizes.c
@@ -3,36 +3,42 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char* argv[]){
 
 	// Declaração da matriz
-	int matriz[3][3];
+	// Para fins de comodidade, declaramos uma matriz quadrada.
+	int tamanho = 3;
+
+	int matriz[tamanho][tamanho];
 
 	// Declaração das variáveis para utilização no for
 	int linha, coluna;
 	int valor = 0;
 
 	// Populamos a matriz com múltiplos de 5
-	for (linha = 0; linha < 3; linha++){
-		for (coluna = 0; coluna < 3; coluna++){
+	for (linha = 0; linha < tamanho; linha++){
+		for (coluna = 0; coluna < tamanho; coluna++){
 			matriz[linha][coluna] = valor;
 			valor += 5;
 		}
 	}
 
 	// Mostramos o conteúdo da matriz
-	for (linha = 0; linha < 3; linha++){
-		for (coluna = 0; coluna < 3; coluna++){
+	for (linha = 0; linha < tamanho; linha++){
+		for (coluna = 0; coluna < tamanho; coluna++){
 			printf("linha %d, coluna %d: %d\n", linha, coluna, matriz[linha][coluna]);
 		}
 	}
 
 	// Em formato mais gráfico
-	for (linha = 0; linha < 3; linha++){
-		for (coluna = 0; coluna < 3; coluna++){
+	for (linha = 0; linha < tamanho; linha++){
+		for (coluna = 0; coluna < tamanho; coluna++){
 			printf("%2d ", matriz[linha][coluna]);
 		}
 		printf("\n");
 	}
+
+	exit(0);
 }
